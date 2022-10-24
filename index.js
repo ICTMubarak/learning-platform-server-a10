a465bb4d515b28c7d80541df8c116d3739206354
ICTMubarak <engr.md.mubarak.hossain@gmail.com>
2022-10-24 23:11:29 +0600

    Primary setup is complided

--- index.js
+++ index.js
@@ -2,9 +2,15 @@ const express = require('express');
 const app = express();
 const port = process.env.PORT || 5000;
 
+const course = require('./course.json');
+
 app.get('/',(req, res)=>{
     res.send('Learn with mubarak server is running')
 });
+
+app.get('/course', (req, res) => {
+    res.send(course);
+})
 
 app.listen(port, ()=>{
     console.log(`Learn with mubarak server running in port=${port}`);
