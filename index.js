2bb30761bf187f212d3823f2e9922935e3e1e734
ICTMubarak <engr.md.mubarak.hossain@gmail.com>
2022-10-26 19:23:03 +0600

    Some update for finally

--- index.js
+++ index.js
@@ -16,6 +16,13 @@ app.get('/courses', (req, res) => {
     res.send(courses);
 })
 
+app.get('/courses/:id', (req, res) =>{
+    const id = parseInt(req.params.id);
+    console.log('loking cor data for id: ', id);
+    const course =courses.find(corse => corse.id === id) || {};
+    res.send(course);
+})
+
 app.listen(port, ()=>{
     console.log(`Learn with mubarak server running in port=${port}`);
 })
